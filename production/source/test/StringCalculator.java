24f2869694910221ab7a7fc2b6dc8cc5213caf61
Simone Amadio <amadiosimone1996@gmail.com>
2019-11-27 18:47:21 +0100

    Issue 8 solved

--- production/source/test/StringCalculator.java
+++ production/source/test/StringCalculator.java
@@ -9,32 +9,35 @@ public class StringCalculator {
 
     private static Integer lim=1001;
 
-    private static IntStream numberStream(String[] data) {
-        return Arrays.stream(data).mapToInt(i -> Integer.valueOf(i)).filter(x->x<lim);
-    }
+    public static int add(String numbers) {
+        if(numbers.isEmpty()) return 0;
 
+        String delimiters= ",|\n" ;
+        if (numbers.startsWith("//")) {
+            delimiters = setDelimiters(numbers);
+            numbers= excludeDelimitersDeclaration(numbers);
+        }
+
+        if(numbers.length()==1) return Integer.valueOf(numbers);
+
+        String[] data=numbers.split(delimiters);
+
+        List<Integer> neg = negatives(data);
+        if (neg.isEmpty()==false) {
+            throw new IllegalArgumentException("Negatives not allowed: "+ neg.toString());
+        }
+
+        return numberStream(data).sum();
+
+    }
 
     private static List<Integer> negatives(String[] data) {
         return numberStream(data).filter(x -> x < 0).boxed().collect(Collectors.toList());
     }
 
-    private static boolean singleCustomSeparator(String s){
-        return s.contains("[")==false;
+    private static IntStream numberStream(String[] data) {
+        return Arrays.stream(data).mapToInt(i -> Integer.valueOf(i)).filter(x->x<lim);
     }
-
-    private static String deleteLastSquareBracket(String s){
-        return s.substring(0,s.length()-1);
-    }
-
-    private static String extractDelimitersDeclaration(String s){
-        return s.substring(2, s.indexOf("\n"));
-    }
-
-    private static String excludeDelimitersDeclaration(String numbers){
-        return numbers.substring(numbers.indexOf("\n")+1,numbers.length());
-    }
-
-
 
     private static String setDelimiters(String numbers) {
         String delimiter = extractDelimitersDeclaration(numbers);
@@ -58,30 +61,20 @@ public class StringCalculator {
 
     }
 
-
-
-
-    public static int add(String numbers) {
-        if(numbers.isEmpty()) return 0;
-
-        String delimiters= ",|\n" ;
-        if (numbers.startsWith("//")) {
-            delimiters = setDelimiters(numbers);
-            numbers= excludeDelimitersDeclaration(numbers);
-        }
-
-        if(numbers.length()==1) return Integer.valueOf(numbers);
-
-        String[] data=numbers.split(delimiters);
-
-        List<Integer> neg = negatives(data);
-        if (neg.isEmpty()==false) {
-            throw new IllegalArgumentException("Negatives not allowed: "+ neg.toString());
-        }
-
-        return numberStream(data).sum();
-
+    private static boolean singleCustomSeparator(String s){
+        return s.contains("[")==false;
     }
 
+    private static String deleteLastSquareBracket(String s){
+        return s.substring(0,s.length()-1);
+    }
+
+    private static String extractDelimitersDeclaration(String s){
+        return s.substring(2, s.indexOf("\n"));
+    }
+
+    private static String excludeDelimitersDeclaration(String numbers){
+        return numbers.substring(numbers.indexOf("\n")+1,numbers.length());
+    }
 
 }
